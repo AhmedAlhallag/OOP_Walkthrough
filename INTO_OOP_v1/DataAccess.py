aa6f404892d49fe235b53e7b454b1f1965f09ba6
Ahmed Alhallag <aalhallag@gmail.com>
2022-10-10 16:23:22 +0200

    REFACTORING INTO OOP: 1st refactor [bad design] list being sent to write/findBy when called

--- INTO_OOP_v1/DataAccess.py
+++ INTO_OOP_v1/DataAccess.py
@@ -44,7 +44,45 @@ class DataAccess:
     --> removing globals: solution 2 using OOP (using instances, instance variables and maintaining a state)
     """
     
+    
+    def __init__(self, filename):
+        """
+        we pass the stored datafile path once and gets stored forever in the to-be-created object lifetime
+        1. we initialize once 
+        2. this data is stuck to the backgroud/pool-of-variables of this object, we can access anytime [maintaining state -> this object has state; STATEFUL]
+        3. accessible ONLY by the object's methods! -> [Access control/Abstraction]
+        """
+        self.stored_datafile_path = self.prepare_connection(filename)
+    
+    def prepare_connection(self, filename):
+        """
+        [Activity]: 
+        """
+        stored_file_path = cwd + "\\" + filename
+        return stored_file_path
+    
+    def read(self):
+        """
+        SR/Cohisive function: doing one thing
+        """
+        with open(self.stored_datafile_path,'r') as afile:
+            return json.load(afile)
+    
+    """
+        [BAD DESIGN]'
+        do you think that there is something off with "alist" arg that we are passing to write and findBy, EVERYTIME we call them?
+        Is there a better refactor? what can we do to fully utilize the classes/instances concept?
+    """
+    
+    def write(self,alist):
+        with open(self.stored_datafile_path,'w') as afile:
+            return json.dump(alist,afile)
 
-    pass
+
+    def findBy(self, key, value, alist):
+        for i, row in enumerate(alist):
+            if row[key] == value:
+                return i 
+        return -1
 
     
