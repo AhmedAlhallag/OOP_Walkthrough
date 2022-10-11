21e8caae61fc0560310c9f6a720a657b028bab48
Ahmed Alhallag <aalhallag@gmail.com>
2022-10-11 22:59:49 +0200

    REFACTORING INTO OOP: [BAD DESIGN] Sol1: storing a refrence to LogicClass in order to access the register method from

--- INTO_OOP_v1/UserInterfaceClass.py
+++ INTO_OOP_v1/UserInterfaceClass.py
@@ -1,3 +1,5 @@
+import sys
+from LogicClass import LogicClass
 class UserInterface:
     """
     
@@ -8,7 +10,33 @@ class UserInterface:
     def __init__(self):
         """
         [Question]: is there anything that needs to be initialzed here?
-        Do wee need the constructor in anything?
+        Do we need the constructor in anything?
+        
+            [Bad Design]
+        Solution1: We need the LogicClass to access the register method from it
+        We can store a copy of the object inside the UserInterface constructor 
+        
+        
+        Why is this bad?
+        1. enforcing Composition , where it is logically not needed -> tight coupled
+        2. Does 'storing' a reference of logicClass is really necessery here? 
         """
-        pass 
+
+        self.logicObj = LogicClass() # Composition again!! (tight coupling)
+    
+    def handle_register_inputs(self):
+        un = input("Enter your un: ")
+        ps  = input("Enter your ps: ")
+        return un, ps
+
+
+
+    def handle_register(self, un, ps):
+        if self.logicObj.register(un, ps):
+            return "Your account has been created", print
+        return "[ERROR] An account with this name already exists.", sys.exit
+
+
+    
+    
         
