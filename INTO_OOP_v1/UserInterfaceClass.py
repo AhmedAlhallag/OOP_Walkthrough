88fac632dcd248359ce776a337ec83db35005a87
Ahmed Alhallag <aalhallag@gmail.com>
2022-10-11 22:51:18 +0200

    REFACTORING INTO OOP: Do we need the constructor in anything here?

--- INTO_OOP_v1/UserInterfaceClass.py
+++ INTO_OOP_v1/UserInterfaceClass.py
@@ -2,5 +2,13 @@ class UserInterface:
     """
     
     [Activity]: attempt to implement this class based on the procidural version
+    Note: We now no longer to 'initialze' any list! we seperated that concern
+    And placed it into LogicClass
     """
-    pass
+    def __init__(self):
+        """
+        [Question]: is there anything that needs to be initialzed here?
+        Do wee need the constructor in anything?
+        """
+        pass 
+        
